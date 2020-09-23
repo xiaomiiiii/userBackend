53b59f5b47f395bcb39e961fb434b647298b3ef1
xiaomiiiii <liuxiaomisept@gmail.com>
2020-09-22 22:09:40 -0700

    Develop user register logic

---
+++ src/main/java/accountUI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class accountUI {
+    public static void userInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter you email address");
+        String email = sc.nextLine();
+        System.out.println("Please enter you name");
+        String name = sc.nextLine();
+        System.out.println("Please enter you password");
+        String password = sc.nextLine();
+        System.out.println("Please enter the year of your birthday");
+        int year = Integer.valueOf(sc.nextLine());
+        System.out.println("Please enter the month of your birthday");
+        int month = Integer.valueOf(sc.nextLine());
+        System.out.println("Please enter the day of your birthday");
+        int day = Integer.valueOf(sc.nextLine());
+        System.out.println("Please enter your gender. 1 is female, 2 is male, 3 is prefer not to say");
+        int gender = Integer.valueOf(sc.nextLine());
+
+        User user = new User(email, name, password, year, month, day, gender);
+
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,84 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void setEmail() {
+        User user1 = new User("gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        String email1 = user1.getEmail();
+        Assert.assertEquals(null, email1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        String email2 = user2.getEmail();
+        Assert.assertEquals("liuxiaomi@gmail.com", email2);
+    }
+
+    @Test
+    public void setPassword() {
+        User user1 = new User("liuxiaomi@gmail.com", "xiaomi", "x", 2020, 1, 1, 1);
+        String password1 = user1.getPassword();
+        Assert.assertEquals(null, password1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        String password2 = user2.getPassword();
+        Assert.assertEquals("xiaomi", password2);
+    }
+
+    @Test
+    public void setName() {
+        User user1 = new User("liuxiaomi@gmail.com", "x", "xiaomi", 2020, 1, 1, 1);
+        String name1 = user1.getName();
+        Assert.assertEquals(null, name1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2000, 1, 1, 1);
+        String name2 = user2.getName();
+        Assert.assertEquals("xiaomi", name2);
+    }
+
+    @Test
+    public void setGender() {
+        User user1 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 0);
+        int gender1 = user1.getGender();
+        Assert.assertEquals(-1, gender1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        int gender2 = user2.getGender();
+        Assert.assertEquals(1, gender2);
+    }
+
+    @Test
+    public void setYear() {
+        User user1 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 100, 1, 1, 1);
+        int year1 = user1.getYear();
+        Assert.assertEquals(-1, year1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2000, 1, 1, 1);
+        int year2 = user2.getYear();
+        Assert.assertEquals(2000, year2);
+    }
+
+    @Test
+    public void setMonth() {
+        User user1 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 20, 1, 1);
+        int month1 = user1.getMonth();
+        Assert.assertEquals(-1, month1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        int month2 = user2.getMonth();
+        Assert.assertEquals(1, month2);
+    }
+
+    @Test
+    public void setDay() {
+        User user1 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 90, 1);
+        int day1 = user1.getDay();
+        Assert.assertEquals(-1, day1);
+
+        User user2 = new User("liuxiaomi@gmail.com", "xiaomi", "xiaomi", 2020, 1, 1, 1);
+        int day2 = user2.getDay();
+        Assert.assertEquals(1, day2);
+    }
+}
